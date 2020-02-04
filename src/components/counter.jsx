9260d814f8f3c19174809aa34595e7a7211df5de
Harun <kaknjo.harun2019@gmail.com>
2020-02-04 15:10:56 -0800

    Dodan decrement button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,37 +1,25 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.getBagdeClassesvalue,
-    tags: []
-  };
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-  /* Jedan nacin renderovanja tags niza u listu*/
-  renderTags() {
-    if (this.state.tags.length === 0) return "No tags for render";
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-
-    /* Drugi nacin jeste da koristimo logicko && u kombinaci sa neki drugim iyrayom jer ce rezultat true && i nekog izaza
-    bilo kojeg moze biti string ili broj uvijek biti taj drugi izraz ukoliko je prvi tacan pa se moze i ovako*/
-  }
   render() {
-    this.getBagdeClasses();
     return (
       <div>
         <span className={this.getBagdeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => {
+            this.props.onIncrement(this.props.counter);
+          }}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          className="btn-outline-primary btn-sm m-2"
+          onClick={() => {
+            this.props.onDecrement(this.props.counter);
+          }}
+        >
+          Decrement
         </button>
         <button
           className="btn btn-danger btn-sm m-2"
@@ -39,21 +27,18 @@ class Counter extends Component {
         >
           Delete{" "}
         </button>
-        {this.state.tags.length === 0 && "No tags for show"}
-        {this.renderTags()}
       </div>
     );
   }
   getBagdeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
-
 export default Counter;
